390cbb8b1c7e19b91e9e6c87f85386dec4c9951a
Konstantin F <fofanoff@gmail.com>
2019-05-26 10:56:33 +0000

    Create the login page. (closes #5)

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const port = process.env.BIRDER_PORT;
+const port = process.env.TWILONE_PORT;
 
 const sequelize = new Sequelize({
-    host: process.env.BIRDER_DB_HOST,
-    port: process.env.BIRDER_DB_PORT,
-    username: process.env.BIRDER_DB_USER,
-    password: process.env.BIRDER_DB_PASSWORD,
-    database: process.env.BIRDER_DB_NAME,
-    dialect: process.env.BIRDER_DB_DIALECT
+  host: process.env.TWILONE_DB_HOST,
+  port: process.env.TWILONE_DB_PORT,
+  username: process.env.TWILONE_DB_USER,
+  password: process.env.TWILONE_DB_PASSWORD,
+  database: process.env.TWILONE_DB_NAME,
+  dialect: process.env.TWILONE_DB_DIALECT
 });
 
 const Twit = sequelize.define('twit', {
-    'message' : {
-        'type' : Sequelize.STRING,
-        'allowNull' : false
-    }
+	'message' : {
+		'type' : Sequelize.STRING,
+		'allowNull' : false
+	}
 });
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (_, response) => {
-    Twit.findAll().then(results => {
-        response.render('index', { 'twits': results });
-    }).catch(error => {
-        console.error(error);
-        response.status(500).end();
-    });
+	Twit.findAll().then(results => {
+	  	response.render('index', { 'twits' : results });
+	}).catch(error => {
+		console.log(error);
+		response.status(500).end();
+	});
 });
 
 app.post('/', (request, response) => {
-    Twit.create({ 'message' : request.body.message }).then(() => {
-        response.redirect('/');
-    }).catch(error => {
-        console.error(error);
-        response.status(500).end();
-    });
+	Twit.create({ 'message' : request.body.message }).then(() => {
+	  	response.redirect('/');
+	}).catch(error => {
+		console.log(error);
+		response.status(500).end();
+	});
 });
 
 sequelize.sync().then(() => {
-    app.listen(port, () => console.log(`Birder is listening on port ${port}.`));
+	app.listen(port, () => console.log(`Twilone is listening on port ${port}.`));
 });
